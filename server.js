b1895a107718b3310ede4e7c696773466b01973d
Ziwa Mukungu <zmukungu@gmail.com>
2018-05-11 01:57:12 -0400

    changed server listen to process.env.PORT

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.get('/', function(req,res) {
 	res.sendFile(path.resolve(__dirname, 'build','index.html'));
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT||3000, function() {
 	console.log("server is listening on port 3000");
 })
