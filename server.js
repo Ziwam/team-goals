2bc5f225ac73d5ab6ca610e6c8d9b23e6fab3ddb
Ziwa Mukungu <zmukungu@gmail.com>
2018-05-11 01:45:40 -0400

    modified package directory to point to build folder

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.resolve(__dirname, '..', 'teamgoals')));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('/', function(req,res) {
-	res.sendFile(path.resolve(__dirname, 'teamgoals','index.html'));
+	res.sendFile(path.resolve(__dirname, 'build','index.html'));
 })
 
 app.listen(3000, function() {
